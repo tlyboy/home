e2206f8874e5d77786e5a546c45be9343d6eefda
Guany <1084072690@qq.com>
2023-11-05 21:32:40 +0800

    feat: Add beian information to footer

---
+++ components/TFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer flex="~ col" items-center p-10 gap-2>
+    <a icon-btn href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">浙ICP备2023022418号-1</a>
+    <a icon-btn flex items-center gap-2 href="https://beian.mps.gov.cn/#/query/webSearch?code=33078402100683" target="_blank" rel="noopener noreferrer">
+      <img h-5 src="~/assets/images/beian.png" alt="beian">
+      <span>浙公网安备 33078402100683号</span>
+    </a>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,4 +4,6 @@
 
     <TNavBar />
   </main>
+
+  <TFooter />
 </template>
