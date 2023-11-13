970fbc45f860b34ae36367f75187dbbddc6261b4
Guany <1084072690@qq.com>
2023-11-13 09:56:11 +0800

    chore: update

--- uno.config.ts
+++ uno.config.ts
@@ -6,19 +6,9 @@ import {
 } from 'unocss'
 
 export default defineConfig({
-  theme: {
-    colors: {
-      brand: {
-        primary: 'var(--brand-primary)',
-        primaryDark: 'var(--brand-primary-dark)',
-        light: 'var(--brand-light)',
-        dark: 'var(--brand-dark)',
-      },
-    },
-  },
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-brand-primary text-white cursor-pointer hover:bg-brand-primary-dark disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-brand-primary !outline-none'],
+    ['btn', 'px-4 py-1 rounded inline-block bg-hex-3498db text-white cursor-pointer hover:bg-hex-2980b9 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-hex-3498db !outline-none'],
   ],
   presets: [
     presetUno(),
