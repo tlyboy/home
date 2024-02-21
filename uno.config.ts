e2605d663fddcb03ed43017ca83c34a8e72daa23
Guany <1084072690@qq.com>
2024-02-21 15:36:08 +0800

    chore: update

--- uno.config.ts
+++ uno.config.ts
@@ -3,12 +3,14 @@ import {
   presetAttributify,
   presetIcons,
   presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
 } from 'unocss'
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-hex-3498db text-white cursor-pointer hover:bg-hex-2980b9 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-hex-3498db !outline-none'],
+    ['btn', 'px-4 py-1 rounded inline-block bg-hex-3498db text-white cursor-pointer !outline-none hover:bg-hex-2980b9 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-hex-3498db'],
   ],
   presets: [
     presetUno(),
@@ -16,5 +18,9 @@ export default defineConfig({
     presetIcons({
       scale: 1.2,
     }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
   ],
 })
