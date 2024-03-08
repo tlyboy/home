0a2f74e5189e0eb4f746eb9628e3dab1dda16efa
Guany <1084072690@qq.com>
2024-03-08 12:00:49 +0800

    chore: update

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="fixed z-50 w-full border-b-2 border-gray-200 bg-white dark:border-black dark:bg-black"
+    class="fixed z-50 w-full border-b-2 border-gray-200 bg-white dark:border-black dark:bg-[#121212]"
   >
     <section
       class="mx-auto flex h-16 max-w-7xl items-center justify-between px-4"
