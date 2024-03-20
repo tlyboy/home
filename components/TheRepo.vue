abe871a85e96f5f02e5a985a2d267bf5e2d18d59
Guany <1084072690@qq.com>
2024-03-20 14:34:25 +0800

    fix: 调整内容

--- components/TheRepo.vue
+++ components/TheRepo.vue
@@ -11,25 +11,30 @@ const { data } = useFetch<Repos>('/github/users/tlyboy/repos', {
 
 <template>
   <div class="flex flex-col items-center justify-center px-4 py-10">
-    <h2 class="mb-4 text-2xl font-bold">我的应用程序</h2>
-    <div class="grid gap-4 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+    <h2 class="mb-10 text-2xl font-bold">我的应用程序</h2>
+    <div class="grid gap-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       <div
         v-for="repo in data"
         :key="repo.id"
-        class="flex flex-col gap-2 rounded-lg border-2 border-gray-200 p-4 dark:border-black"
+        class="flex flex-col gap-2 rounded-lg border-2 border-gray-200 p-4 dark:border-gray-700"
       >
-        <a :href="repo.html_url" target="_blank" rel="noopener noreferrer">
-          <h3 class="text-xl font-bold">{{ repo.name }}</h3>
-        </a>
+        <h3 class="font-bold">{{ repo.name }}</h3>
         <p>{{ repo.description }}</p>
-        <a
-          v-if="repo.homepage"
-          :href="repo.homepage"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="text-blue-500"
-          >{{ repo.homepage }}</a
-        >
+        <div class="flex justify-end gap-2">
+          <a
+            v-if="repo.homepage"
+            class="icon-btn i-carbon-link"
+            :href="repo.homepage"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+          <a
+            class="icon-btn i-carbon-logo-github"
+            :href="repo.html_url"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+        </div>
       </div>
     </div>
   </div>
