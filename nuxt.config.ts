4f93311fbf0645aab7b6bbbe347c0e709c6db491
Guany <1084072690@qq.com>
2025-07-16 18:44:03 +0800

    chore: 更新 Nuxt 配置中的兼容性日期至 2025-07-16，升级 Nuxt 版本至 4.0.0，更新相关依赖项版本以保持一致性

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: '2025-07-16',
   devtools: { enabled: true },
-  future: {
-    compatibilityVersion: 4,
-  },
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
